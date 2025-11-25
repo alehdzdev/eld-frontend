b053f2fa35fbab2683b4d6f1baa8112a21b8f9d6
Alejandro Hernandez <alehdzdev@gmail.com>
2025-11-24 23:28:04 -0400

    [FIX] marker

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,17 @@
 import { useEffect, useRef } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 export default function RouteMap({ points = [] }) {
   const mapRef = useRef(null);
